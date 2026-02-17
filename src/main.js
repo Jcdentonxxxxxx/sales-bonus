8ca7f1ae7cc676d05ed93dd10422d070e13bb074
Vitaly Kuzmenko <jcdentonxxxxxx@yandex.ru>
2026-02-18 02:51:19 +0300

    добавлена проверка на пустые массивы в данных

--- src/main.js
+++ src/main.js
@@ -48,7 +48,11 @@ function analyzeSalesData(data, options) {
     !Array.isArray(data.customers) ||
     !Array.isArray(data.products) ||
     !Array.isArray(data.sellers) ||
-    !Array.isArray(data.purchase_records)
+    !Array.isArray(data.purchase_records) ||
+    data.customers.length === 0 ||
+    data.products.length === 0 ||
+    data.sellers.length === 0 ||
+    data.purchase_records.length === 0
   ) {
     throw new Error("Некорректные входные данные");
   }
